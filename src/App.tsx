77ddff1f729ecf542749569db356058476551a72
Aman Irshad <aman.itz.me07@gmail.com>
2025-10-15 23:44:46 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import Index from "./pages/index";
 import Products from "./pages/Products";
 import SattuPowder from "./pages/SattuPowder";
 import ReadyToDrink from "./pages/ReadyToDrink";
